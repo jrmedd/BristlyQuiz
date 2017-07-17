5f842c23fc04fc654cfa58e5ae6666fca64363ee
jrmedd <hello@jamesmedd.co.uk>
2017-07-17 12:04:55 +0100

    Add quiz answering

--- js/quizLogic.js
+++ js/quizLogic.js
@@ -1,13 +1,55 @@
 chrome.serial.getDevices(onGetDevices);
 
-$('.team-advance').on('click', function(e){
-  var team = "move_team('" + $(this).val() + "')\r";
-  console.log(team);
-  writeSerial(team);
+var teams;
+var currentTeam;
+var currentQuestion;
+var progress = 0;
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function advanceTeam(team) {
+  var teamCommand = "move_team('" + team + "')\r";
+  writeSerial(teamCommand);
+}
+
+$.getJSON(chrome.runtime.getURL('js/teams.json'), function(teamInfo) {
+  teams = teamInfo.teams;
 });
 
-$.getJSON(chrome.runtime.getURL('js/questions.json'), function(questions) {
-  $.each(questions, function(idx, value){
-    console.log(value.question);
+function displayQuestion() {
+  $.getJSON(chrome.runtime.getURL('js/questions.json'), function(questions) {
+    currentTeam = teams[progress%teams.length];
+    currentQuestion = questions[progress];
+    $("#question").html("<h3>For team "+currentTeam+":<br>"+currentQuestion.question+"</h3>");
+    var randomIndex = getRandomArbitrary(0, currentQuestion.incorrect.length)|0;
+    $("#answers").empty();
+    $.each(currentQuestion.incorrect, function(idx, value){
+      if (idx == randomIndex) {
+        $("#answers").append('<a class="correct" href="#">'+currentQuestion.correct+'</a><br>');
+      }
+      $("#answers").append('<a class="incorrect" href="#">'+value+'</a><br>');
+    });
   });
+}
+
+$("#answers").on('click', function(e){
+  e.preventDefault();
+  var clickedAnswer = $(e.target);
+  if (clickedAnswer.hasClass('correct')) {
+    advanceTeam(currentTeam);
+    $("#question").html("<h3>That's correct! You move for 2 seconds!</h3>");
+  }
+  else if (clickedAnswer.hasClass('incorrect')) {
+    $("#question").html("<h3>Too bad, everyone else moves, you stay still!</h3>");
+    $.each(teams, function(idx, teamName) {
+      if (teamName != currentTeam) {
+        advanceTeam(teamName);
+      }
+    });
+  }
+  setTimeout(function() {progress ++; displayQuestion();}, 2000);
 });
+
+displayQuestion();
