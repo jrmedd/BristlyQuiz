53d2b9feafaabe781b5c917b221638eb7c93b772
jrmedd <hello@jamesmedd.co.uk>
2018-05-22 09:40:17 +0100

    MOAR questions

--- microbit_code/botCode.py
+++ microbit_code/botCode.py
@@ -2,7 +2,7 @@ import microbit
 import radio
 
 #set a device ID
-this_device = "A"
+this_device = "D"
 
 radio.config(power=7, channel=22)
 radio.on() #start radio comms
@@ -11,6 +11,10 @@ microbit.pin2.write_digital(0)
 while True:
     if microbit.button_a.is_pressed():
         microbit.display.show(this_device) #display this device's ID if button A is pressed
+    elif microbit.button_b.is_pressed():
+        microbit.pin2.write_digital(1)
+        microbit.sleep(5000)
+        microbit.pin2.write_digital(0)
     else:
         microbit.display.clear()
     received_messages = radio.receive() #check for messages over radio
